58dd2b0425cbcf52d3a38d68644d4db4600434fd
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-11-18 13:29:14 +0100

    main page for guest prototype

--- RoomReservationSystem/Program.cs
+++ RoomReservationSystem/Program.cs
@@ -16,7 +16,7 @@ namespace RoomReservationSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainPageGuest());
         }
     }
 }
