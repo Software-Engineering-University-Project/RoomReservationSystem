a110f6e8fcba325460495b1382f51921e1b23464
AnnaJust <anna.just@op.pl>
2021-12-11 17:38:40 +0100

    database connection prototype

--- RoomReservationSystem/Program.cs
+++ RoomReservationSystem/Program.cs
@@ -1,6 +1,9 @@
 ﻿using RoomReservationSystem.UserInterface;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +21,40 @@ namespace RoomReservationSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new RoomReservationSystem.UserInterface.RoomReservationSystem());
+
+            string provider = ConfigurationManager.AppSettings["provider"];
+
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if(connection == null)
+                {
+                    Debug.WriteLine("connection error");
+                    return;
+                }
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+                if(command == null)
+                {
+                    Debug.WriteLine("connection error");
+                    return;
+                }
+                command.Connection = connection;
+                command.CommandText = "Select * from PersonalData";
+
+                using(DbDataReader dataReader = command.ExecuteReader())
+                {
+                    while(dataReader.Read())
+                    {
+                        Debug.WriteLine($"{dataReader["FirstName"]}");
+                    }
+                }
+            }
         }
     }
 }
