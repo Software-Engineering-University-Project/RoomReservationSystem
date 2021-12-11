cd0d7ba225123c39983514d24e4ef5f9c1d69060
PaulinaCzapla <paulina.czp00@gmail.com>
2021-12-11 14:03:16 +0100

    Created make reservation view

--- RoomReservationSystem/UserInterface/FormMakeReservation.Designer.cs
+++ RoomReservationSystem/UserInterface/FormMakeReservation.Designer.cs
@@ -29,12 +29,211 @@ namespace RoomReservationSystem.UserInterface
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateFrom = new System.Windows.Forms.DateTimePicker();
+            this.roomId = new System.Windows.Forms.TextBox();
+            this.userId = new System.Windows.Forms.TextBox();
+            this.makeReservationButton = new System.Windows.Forms.Button();
+            this.labelUserID = new System.Windows.Forms.Label();
+            this.labelRoomId = new System.Windows.Forms.Label();
+            this.dateTo = new System.Windows.Forms.DateTimePicker();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 261F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 348F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 363F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 347F));
+            this.tableLayoutPanel1.Controls.Add(this.label9, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.roomId, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.makeReservationButton, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.labelUserID, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelRoomId, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.dateFrom, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.dateTo, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.userId, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.labelPrice, 1, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 113F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 87F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 143F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 231F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1416, 842);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(264, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "From";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label9.Location = new System.Drawing.Point(708, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 29);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "To";
+            // 
+            // dateFrom
+            // 
+            this.dateFrom.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.dateFrom.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.dateFrom.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateFrom.Location = new System.Drawing.Point(264, 158);
+            this.dateFrom.Name = "dateFrom";
+            this.dateFrom.Size = new System.Drawing.Size(342, 32);
+            this.dateFrom.TabIndex = 4;
+            this.dateFrom.ValueChanged += new System.EventHandler(this.dateFrom_ValueChanged);
+            // 
+            // roomId
+            // 
+            this.roomId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.roomId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.roomId.Dock = System.Windows.Forms.DockStyle.Top;
+            this.roomId.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.roomId.ForeColor = System.Drawing.Color.Gainsboro;
+            this.roomId.Location = new System.Drawing.Point(708, 293);
+            this.roomId.Name = "roomId";
+            this.roomId.Size = new System.Drawing.Size(357, 25);
+            this.roomId.TabIndex = 7;
+            this.roomId.TextChanged += new System.EventHandler(this.roomId_TextChanged);
+            // 
+            // userId
+            // 
+            this.userId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.userId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.userId.Dock = System.Windows.Forms.DockStyle.Top;
+            this.userId.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.userId.ForeColor = System.Drawing.Color.Gainsboro;
+            this.userId.Location = new System.Drawing.Point(264, 293);
+            this.userId.Name = "userId";
+            this.userId.Size = new System.Drawing.Size(342, 25);
+            this.userId.TabIndex = 8;
+            this.userId.TextChanged += new System.EventHandler(this.userId_TextChanged);
+            // 
+            // makeReservationButton
+            // 
+            this.makeReservationButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.makeReservationButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.makeReservationButton.FlatAppearance.BorderSize = 0;
+            this.makeReservationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.makeReservationButton.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.makeReservationButton.ForeColor = System.Drawing.Color.Gainsboro;
+            this.makeReservationButton.Location = new System.Drawing.Point(800, 450);
+            this.makeReservationButton.Name = "makeReservationButton";
+            this.makeReservationButton.Size = new System.Drawing.Size(265, 80);
+            this.makeReservationButton.TabIndex = 10;
+            this.makeReservationButton.Text = "Reservate";
+            this.makeReservationButton.UseVisualStyleBackColor = false;
+            this.makeReservationButton.Click += new System.EventHandler(this.makeReservationButton_Click);
+            // 
+            // labelUserID
+            // 
+            this.labelUserID.AutoSize = true;
+            this.labelUserID.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelUserID.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelUserID.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelUserID.Location = new System.Drawing.Point(264, 261);
+            this.labelUserID.Name = "labelUserID";
+            this.labelUserID.Size = new System.Drawing.Size(342, 29);
+            this.labelUserID.TabIndex = 13;
+            this.labelUserID.Text = "User id";
+            // 
+            // labelRoomId
+            // 
+            this.labelRoomId.AutoSize = true;
+            this.labelRoomId.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelRoomId.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelRoomId.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelRoomId.Location = new System.Drawing.Point(708, 261);
+            this.labelRoomId.Name = "labelRoomId";
+            this.labelRoomId.Size = new System.Drawing.Size(357, 29);
+            this.labelRoomId.TabIndex = 16;
+            this.labelRoomId.Text = "Room id";
+            // 
+            // dateTo
+            // 
+            this.dateTo.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.dateTo.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.dateTo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dateTo.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTo.Location = new System.Drawing.Point(708, 158);
+            this.dateTo.Name = "dateTo";
+            this.dateTo.Size = new System.Drawing.Size(357, 32);
+            this.dateTo.TabIndex = 19;
+            this.dateTo.ValueChanged += new System.EventHandler(this.dateTo_ValueChanged);
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPrice.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelPrice.Location = new System.Drawing.Point(264, 433);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(342, 100);
+            this.labelPrice.TabIndex = 20;
+            this.labelPrice.Text = "Price";
+            this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMakeReservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(1416, 842);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormMakeReservation";
             this.Text = "FormMakeReservation";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox roomId;
+        private System.Windows.Forms.Button makeReservationButton;
+        private System.Windows.Forms.Label labelUserID;
+        private System.Windows.Forms.Label labelRoomId;
+        private System.Windows.Forms.DateTimePicker dateFrom;
+        private System.Windows.Forms.DateTimePicker dateTo;
+        private System.Windows.Forms.TextBox userId;
+        private System.Windows.Forms.Label labelPrice;
     }
 }
--- RoomReservationSystem/UserInterface/FormMakeReservation.cs
+++ RoomReservationSystem/UserInterface/FormMakeReservation.cs
@@ -16,5 +16,57 @@ namespace RoomReservationSystem.UserInterface
         {
             InitializeComponent();
         }
+
+
+        private void GetUserData()
+        {
+         // dateFrom
+         // dateTo
+         // userId
+         //roomId
+        }
+
+        public void EnableClientPermissions()
+        {
+            userId.Visible = false;
+            labelUserID.Visible= false;
+
+            roomId.Visible = false;
+            labelRoomId.Visible = false;
+        }
+
+        public void EnableHighPermissions()
+        {
+            userId.Visible = true;
+            labelUserID.Visible = true;
+
+            roomId.Visible = true;
+            labelRoomId.Visible = true;
+        }
+
+        private void makeReservationButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roomId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateFrom_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTo_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
