923cee9e253c8153098130f6b5617f90da30c628
Paulina Czapla <paulina.czp00@gmail.com>
2022-02-03 23:41:10 +0100

    Added listView

--- RoomReservationSystem/UserInterface/FormTableView.Designer.cs
+++ RoomReservationSystem/UserInterface/FormTableView.Designer.cs
@@ -30,10 +30,8 @@ namespace RoomReservationSystem.UserInterface
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.table = new System.Windows.Forms.TableLayoutPanel();
-            this.panelWithTable = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.tableLayoutPanel1.SuspendLayout();
-            this.panelWithTable.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -43,7 +41,7 @@ namespace RoomReservationSystem.UserInterface
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1236F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.panelWithTable, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 1, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -55,36 +53,19 @@ namespace RoomReservationSystem.UserInterface
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1398, 810);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // table
+            // listView1
             // 
-            this.table.AutoSize = true;
-            this.table.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.table.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.table.ColumnCount = 1;
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.table.Dock = System.Windows.Forms.DockStyle.Top;
-            this.table.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.table.ForeColor = System.Drawing.Color.Gainsboro;
-            this.table.Location = new System.Drawing.Point(0, 0);
-            this.table.Name = "table";
-            this.table.RowCount = 1;
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.table.Size = new System.Drawing.Size(1230, 20);
-            this.table.TabIndex = 0;
-            // 
-            // panelWithTable
-            // 
-            this.panelWithTable.AutoScroll = true;
-            this.panelWithTable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.panelWithTable.Controls.Add(this.table);
-            this.panelWithTable.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelWithTable.Location = new System.Drawing.Point(42, 64);
-            this.panelWithTable.Name = "panelWithTable";
-            this.panelWithTable.Size = new System.Drawing.Size(1230, 660);
-            this.panelWithTable.TabIndex = 1;
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listView1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(42, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1230, 660);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // FormTableView
             // 
@@ -96,8 +77,6 @@ namespace RoomReservationSystem.UserInterface
             this.Name = "FormTableView";
             this.Text = "RESERVATIONS";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.panelWithTable.ResumeLayout(false);
-            this.panelWithTable.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -105,7 +84,6 @@ namespace RoomReservationSystem.UserInterface
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel table;
-        private System.Windows.Forms.Panel panelWithTable;
+        private System.Windows.Forms.ListView listView1;
     }
 }
